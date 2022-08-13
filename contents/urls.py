2d3d04999a3693e665c3a89617ecdaa04bf0b685
s13g <ayflix0@gmail.com>
2022-08-14 00:08:42 +0100

    4th commit

--- contents/urls.py
+++ contents/urls.py
@@ -3,6 +3,9 @@ from django.urls import path
 from contents import views
 
 urlpatterns = [
-    path("", views.index, name='index'),
-    path("concept", views.concept, name="concept"),
+    path("", views.concept, name='concept'),
+    path("box-house/", views.box_house, name="box_house"),
+    path("short-term/", views.short_term, name="short_term"),
+    path("long-term/", views.long_term, name="long_term"),
+    path("box-map/", views.box_map, name="box_map")
 ]
--- contents/views.py
+++ contents/views.py
@@ -4,16 +4,25 @@ from django.shortcuts import render
 from contents.models import Content
 
 
-def index(request):
-    return render(request, 'home.html')
-
-
 def concept(request):
-    content = Content.objects.get(pk=2)
-    return render(request, 'test.html', context={"content": content})
+    content = Content.objects.get(pk=1)
+    return render(request, 'home.html', context={"concept": content})
 
 
-def box(request):
-    boxes = Content.objects.get(pk=4)
-    return render(request, 'test.html', context={"boxes": boxes})
+def box_house(request):
+    boxes = Content.objects.get(pk=2)
+    return render(request, 'box_house.html', context={"boxes": boxes})
 
+
+def short_term(request):
+    shorts = Content.objects.get(pk=3)
+    return render(request, 'short_term.html', context={"shorts": shorts})
+
+
+def long_term(request):
+    long = Content.objects.get(pk=4)
+    return render(request, 'long_term.html', context={"long": long})
+
+
+def box_map(request):
+    return render(request, 'box_map.html')
