e4aebbe998c895bd9a686ecd87a2d48bbd5fb5d3
s13g <ayflix0@gmail.com>
2022-08-14 09:32:31 +0100

    updated

--- boxheadsnft/settings.py
+++ boxheadsnft/settings.py
@@ -22,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+with open('/home/s13g/W3/secret_key.txt') as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
